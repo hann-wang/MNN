06634b69701c6f7d6466f6ee40115b889b2435cc
Wang Han <wanghan@airdoc.com>
2022-11-07 19:28:11 +0800

    fetch oneDNN from conan

--- cmake/oneDNN.cmake
+++ cmake/oneDNN.cmake
@@ -23,16 +23,4 @@ ExternalProject_Get_Property(oneDNN install_dir)
 include_directories(${ONEDNN_DIR}/include)
 set(dnnl_LIBS ${PROJECT_BINARY_DIR}/lib/libdnnl.a)
 
-function(add_whole_archive_flag lib output_var)
-  if(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-    set(${output_var} -Wl,-force_load,$<TARGET_FILE:${lib}> PARENT_SCOPE)
-  elseif(MSVC)
-    # In MSVC, we will add whole archive in default.
-    set(${output_var} -WHOLEARCHIVE:${lib} PARENT_SCOPE)
-  else()
-    # Assume everything else is like gcc
-    set(${output_var} "-Wl,--whole-archive ${lib} -Wl,--no-whole-archive" PARENT_SCOPE)
-  endif()
-endfunction()
-
 add_whole_archive_flag(dnnl_LIBS ${dnnl_LIBS})
